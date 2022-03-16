163dd7c0528e05335f2693692a5428d07d028a71
gonzaloeiza <gonzaloeizaguirre@opendeusto.es>
2022-03-16 22:03:53 +0100

    acabando reservas

--- src/pages/Booking.js
+++ src/pages/Booking.js
@@ -9,12 +9,14 @@ class Booking extends Component {
     constructor(props) {
         super(props);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.book = this.book.bind(this);
         this.state =  {
             loading: true,
             username: null,
             courts: [],
             bookingDay: new Date().toJSON().slice(0,10),
             selectedCourt: null,
+            selectedCourtId: -1,
             availableTimes: []
         }
     }
@@ -27,6 +29,7 @@ class Booking extends Component {
         }
         await this.setState({
             selectedCourt: selectedCourt.value,
+            selectedCourtId: selectedCourt.selectedIndex - 1,
             loading: true
         });
         const availableTimes = await getDisponibility(this.props, this.state.bookingDay, this.state.selectedCourt);
@@ -34,9 +37,13 @@ class Booking extends Component {
             availableTimes: availableTimes,
             loading: false
         });
-        document.getElementById("court").selectedIndex = selectedCourt.selectedIndex
+        document.getElementById("court").selectedIndex = selectedCourt.selectedIndex;
+        
     }
 
+    async book (e, withLight = true) {
+        console.log(this.state.courts[this.state.selectedCourtId].name + " " + this.state.availableTimes[e.target.value] + " " + withLight);
+    }
 
     async componentDidMount() {
         const courts = await getCourts(this.props);
@@ -65,20 +72,38 @@ class Booking extends Component {
         var availableTimes = [];
         if (this.state.availableTimes.length > 0) {
             for (i = 0; i < this.state.availableTimes.length; i++) {
-                const date = new Date(this.state.availableTimes[i]);
+                var date = new Date(this.state.availableTimes[i]);
+                date.setTime(date.getTime() + new Date().getTimezoneOffset()*60*1000);
                 var hours = date.getHours();
                 var minutes = date.getMinutes();
                 hours = ("0" + hours).slice(-2); //adds a 0 if hour < 10
                 minutes = ("0" + minutes).slice(-2); //adds a 0 if minutes < 10
                 availableTimes.push(
                     <div key={i} className="mb-4 col-xs-12 col-sm-6 col-md-6 col-lg-4">
-                        <div className="card border-dark">
+                        <div className="card bg-light border-dark">
                             <div className="card-header bg-dark">
-                                <h4 className="strong text-white my-auto">{`${hours}:${minutes}`}</h4>
+                                <div className="card-text d-flex row">
+                                    <h4 className="col fw-bold text-white">{`${hours}:${minutes}`}</h4>
+                                    <h4 className="col text-end fw-bold text-white">{this.state.courts[this.state.selectedCourtId].bookReservationTime}'</h4>
+                               </div>
                             </div>
                             <div className="card-body">
-                                <p className="card-text">Precio con luz</p>
-                                <p className="card-text">Precio sin luz</p>
+                                <div className="row">
+                                    <p className="col fw-bold">Precio con luz</p>
+                                    <p className="col text-end fw-bold">{this.state.courts[this.state.selectedCourtId].priceWithLight}€</p>
+                                </div>
+                                <div className="row">
+                                    <p className="col fw-bold">Precio sin luz</p>
+                                    <p className="col text-end fw-bold">{this.state.courts[this.state.selectedCourtId].priceWithoutLight}€</p>
+                                </div>
+                                <div className="row">
+                                    <div className="col-xs-12 col-sm-6 my-2 text-center">
+                                        <button value={i} className="btn btn-success" onClick={(e) => this.book(e)}>reservar con luz</button>
+                                    </div>
+                                    <div className="col-xs-12 col-sm-6 my-2 text-center">
+                                        <button value={i} className="btn btn-success" onClick={(e) => this.book(e, false)}>reservar sin luz</button>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
