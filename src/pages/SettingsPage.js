6d2478d7d6708b8621f0037b7e2eb3af7a2bb120
Gonzalo Eizaguirre <gonzaloeizaguirre@opendeusto.es>
2022-04-16 13:32:32 +0200

    ajustes del usuario

--- src/pages/SettingsPage.js
+++ src/pages/SettingsPage.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
+import { Modal } from 'react-bootstrap';
 import { Layout, BlueCard } from "../component";
 import PhoneInput from 'react-phone-input-2';
-import { getUsername, getUserData } from "../services/user.services";
+import { getUsername, getUserData, updateUserProfile, deleteAccount } from "../services/user.services";
+import { message } from "antd";
 
 class SettingsPage extends Component {
     constructor(props) {
         super(props);
+        this.updateProfile = this.updateProfile.bind(this);
+        this.showDeleteAccountModal = this.showDeleteAccountModal.bind(this);
+        this.hideDeleteAccountModal = this.hideDeleteAccountModal.bind(this);
         this.changeDNI = this.changeDNI.bind(this);
         this.changeName = this.changeName.bind(this);
         this.changeFirstSurname = this.changeFirstSurname.bind(this);
@@ -18,9 +23,11 @@ class SettingsPage extends Component {
         this.changePoblation = this.changePoblation.bind(this);
         this.changePostalCode = this.changePostalCode.bind(this);
         this.changeProvince = this.changeProvince.bind(this);
+        this.deleteProfile = this.deleteProfile.bind(this);
         this.state = {
             loading: true,
             disableEdit: true,
+            showDeleteAccountModal: false,
             username: null,
             user: null,
             selectedGenderIndex: 0,
@@ -48,55 +55,153 @@ class SettingsPage extends Component {
        document.getElementById("gender").selectedIndex = this.state.selectedGenderIndex;
     }
 
-    async changeDNI(e) {
 
+    async showDeleteAccountModal() {
+        await this.setState({
+            showDeleteAccountModal: true
+        });
+    }
+
+    async hideDeleteAccountModal() {
+        await this.setState({
+            showDeleteAccountModal: false
+        });
+    }
+
+
+    async changeDNI(e) {
+        var user = {...this.state.user};
+        user.dni = e.target.value;
+        await this.setState({
+            user: user
+        });
     }
 
     async changeName(e) {
-
+        var user = {...this.state.user};
+        user.name = e.target.value;
+        await this.setState({
+            user: user
+        });
     }
 
     async changeFirstSurname(e) {
-
+        var user = {...this.state.user};
+        user.firstSurname = e.target.value;
+        await this.setState({
+            user: user
+        });
     }
 
     async changeSecondSurname(e) {
-
+        var user = {...this.state.user};
+        user.secondSurname = e.target.value;
+        await this.setState({
+            user: user
+        });
     }
 
     async changeBirthDate(e) {
-
+        var user = {...this.state.user};
+        user.dateBirth = e.target.value;
+        await this.setState({
+            user: user
+        });
     }
 
     async changeGender(e) {
-
+        var user = {...this.state.user};
+        user.gender = e.target.value;
+        await this.setState({
+            user: user
+        });
     }
 
     async changePhoneNumber(e) {
-
+        var user = {...this.state.user};
+        user.phoneNumber = e;
+        await this.setState({
+            user: user
+        });
     }
 
     async changePassword(e) {
-
+        var user = {...this.state.user};
+        user.newPassword = e.target.value;
+        await this.setState({
+            user: user
+        });
     }
 
     async changeDirection(e) {
-
+        var user = {...this.state.user};
+        user.direction = e.target.value;
+        await this.setState({
+            user: user
+        });
     }
 
     async changePoblation(e) {
-
+        var user = {...this.state.user};
+        user.poblation = e.target.value;
+        await this.setState({
+            user: user
+        });
     }
 
     async changePostalCode(e) {
-
+        var user = {...this.state.user};
+        user.postalCode = e.target.value;
+        await this.setState({
+            user: user
+        });
     }
 
     async changeProvince(e) {
-
+        var user = {...this.state.user};
+        user.province = e.target.value;
+        await this.setState({
+            user: user
+        });
     }
 
+    async updateProfile(e) {
+        e.preventDefault();
+        
+        await this.setState({
+            loading: true
+        });
+
+        if (this.state.user.newPassword !== document.getElementById("repeatPassword").value) {
+            message.error("Las contraseñas no coinciden");
+        } else {
+            const successUpdate = await updateUserProfile(this.props, this.state.user);
+            if (successUpdate) {
+
+                const userData = await getUserData(this.props);
+                userData.newPassword = "";                
+                localStorage.setItem("name", userData.name);
+                
+                await this.setState({
+                    loading: false,
+                    disableEdit: true,
+                    user: userData,
+                    username: await getUsername()
+                });
+
+                document.getElementById("repeatPassword").value = "";
+                document.getElementById("gender").selectedIndex = this.state.selectedGenderIndex;        
+            } else {
+                await this.setState({
+                    loading: false
+                });
+            }
+        }
+    }
     
+    async deleteProfile() {
+        await deleteAccount(this.props);
+    }
 
     render() {
         return (
@@ -105,78 +210,97 @@ class SettingsPage extends Component {
                     <h1>Ajustes</h1>
                 </BlueCard>
                 {this.state.user && (
-                    <BlueCard className="col-xs-12 col-sm-12 col-md-10 col-lg-8">
-                        <div className="text-center">
-                            <h1 className="text-center mb-4 text-dark">{this.state.user.name} {this.state.user.firstSurname}</h1>
-                        </div>
-                        <div className="row justify-content-end mb-3 mx-0">
-                            <button className="col-xs-12 col-sm-12 col-md-3 btn btn-secondary" onClick={(e) => this.setState({disableEdit: !this.state.disableEdit})}>
-                                <i className="bi bi-pencil"> Editar perfil</i>
-                            </button>
-                        </div>
-                        <form onSubmit={this.updateProfile}>
-                            <div className="mb-1">
-                                <input type="text" className="form-control" disabled={this.state.disableEdit} value={this.state.user.dni} onChange={this.changeDNI} id="dni" placeholder="DNI *" required/>
-                            </div>
-                            <div className="mb-1">
-                                <input type="text" className="form-control" disabled={this.state.disableEdit} value={this.state.user.name} onChange={this.changeName} id="name" placeholder="Nombre *" required/>
-                            </div>
-                            <div className="mb-1">
-                                <input type="text" className="form-control" disabled={this.state.disableEdit} value={this.state.user.firstSurname} onChange={this.changeFirstSurname} id="firstSurname" placeholder="Primer apellido *" required/>
-                            </div>
-                            <div className="mb-3">
-                                <input type="text" className="form-control" disabled={this.state.disableEdit} value={this.state.user.secondSurname} onChange={this.changeSecondSurname} id="secondSurname" placeholder="Segundo apellido" />
-                            </div>
-                            <div className="mb-1">
-                                <p className='mx-1 my-1'>Fecha de nacimiento</p>
-                                <input type="date" className="form-control" disabled={this.state.disableEdit} value={this.state.user.dateBirth}  onChange={this.changeBirthDate} id="dateBirth" required/>
-                            </div>
-                            <div className='col-md-6 mb-1'>
-                                <div className="input-group">
-                                    <select  className="custom-select form-select" disabled={this.state.disableEdit} onChange={this.changeGender} id="gender" required>
-                                        <option hidden>Sexo</option>
-                                        <option value="HOMBRE">Hombre</option>
-                                        <option value="MUJER">Mujer</option>
-                                        <option value="OTRO">Otro</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div className="mb-3">
-                                <PhoneInput
-                                placeholder="Numero de teléfono"
-                                country={"es"}
-                                disabled={this.state.disableEdit}
-                                value={this.state.user.phoneNumber}
-                                onChange={this.changePhoneNumber}
-                                />
-                            </div>
-                            <div className="mb-1">
-                                <input type="password" className="form-control" disabled={this.state.disableEdit}  onChange={this.changePassword} id="password" placeholder="Nueva contraseña" />
-                            </div>
-                            <div className="mb-3">
-                                <input type="password" className="form-control" disabled={this.state.disableEdit} id="repeatPassword" placeholder="Repetir nueva contraseña" />
-                            </div>
-                            <div className="mb-1">
-                                <input type="text" className="form-control" disabled={this.state.disableEdit} value={this.state.user.direction} onChange={this.changeDirection} id="direction" placeholder="Dirección" />
-                            </div>
-                            <div className="mb-1">
-                                <input type="text" className="form-control" disabled={this.state.disableEdit} value={this.state.user.poblation} onChange={this.changePoblation} id="poblation" placeholder="Población" />
-                            </div>
-                            <div className="mb-1">
-                                <input type="text" className="form-control" disabled={this.state.disableEdit} id="postalCode" value={this.state.user.postalCode} onChange={this.changePostalCode} placeholder="Código postal" />
-                            </div>
-                            <div className="mb-3">
-                                <input type="text" className="form-control" disabled={this.state.disableEdit} id="province" value={this.state.user.province} onChange={this.changeProvince} placeholder="Provincia" />
-                            </div>
+                    <>
+                        <BlueCard className="col-xs-12 col-sm-12 col-md-10 col-lg-8">
                             <div className="text-center">
-                                <button type="submit" disabled={this.state.disableEdit} className="btn btn-primary px-5 w-100">Actualizar datos</button>
+                                <h1 className="text-center mb-4 text-dark">{this.state.user.name} {this.state.user.firstSurname}</h1>
                             </div>
-                        </form>
-                        <div className="text-center">
-                            <button disabled={this.state.disableEdit} onClick={this.showDeleteAccountModal} className="btn btn-danger mt-2 px-5 w-100">Eliminar cuenta</button>
-                        </div>
-                    </BlueCard>
-                )}
+                            <div className="row justify-content-end mb-3 mx-0">
+                                <button className="col-xs-12 col-sm-12 col-md-3 btn btn-secondary" onClick={(e) => this.setState({disableEdit: !this.state.disableEdit})}>
+                                    <i className="bi bi-pencil"> Editar perfil</i>
+                                </button>
+                            </div>
+                            <form onSubmit={this.updateProfile}>
+                                <div className="mb-1">
+                                    <input type="text" className="form-control" disabled={this.state.disableEdit} value={this.state.user.dni} onChange={this.changeDNI} id="dni" placeholder="DNI *" required/>
+                                </div>
+                                <div className="mb-1">
+                                    <input type="text" className="form-control" disabled={this.state.disableEdit} value={this.state.user.name} onChange={this.changeName} id="name" placeholder="Nombre *" required/>
+                                </div>
+                                <div className="mb-1">
+                                    <input type="text" className="form-control" disabled={this.state.disableEdit} value={this.state.user.firstSurname} onChange={this.changeFirstSurname} id="firstSurname" placeholder="Primer apellido *" required/>
+                                </div>
+                                <div className="mb-3">
+                                    <input type="text" className="form-control" disabled={this.state.disableEdit} value={this.state.user.secondSurname} onChange={this.changeSecondSurname} id="secondSurname" placeholder="Segundo apellido" />
+                                </div>
+                                <div className="mb-1">
+                                    <p className='mx-1 my-1'>Fecha de nacimiento</p>
+                                    <input type="date" className="form-control" disabled={this.state.disableEdit} value={this.state.user.dateBirth}  onChange={this.changeBirthDate} id="dateBirth" required/>
+                                </div>
+                                <div className='col-md-6 mb-1'>
+                                    <div className="input-group">
+                                        <select  className="custom-select form-select" disabled={this.state.disableEdit} onChange={this.changeGender} id="gender" required>
+                                            <option hidden>Sexo</option>
+                                            <option value="HOMBRE">Hombre</option>
+                                            <option value="MUJER">Mujer</option>
+                                            <option value="OTRO">Otro</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div className="mb-3">
+                                    <PhoneInput
+                                    placeholder="Numero de teléfono"
+                                    country={"es"}
+                                    disabled={this.state.disableEdit}
+                                    value={this.state.user.phoneNumber}
+                                    onChange={this.changePhoneNumber}
+                                    />
+                                </div>
+                                <div className="mb-1">
+                                    <input type="password" className="form-control" disabled={this.state.disableEdit} value={this.state.user.newPassword} onChange={this.changePassword} id="password" placeholder="Nueva contraseña" />
+                                </div>
+                                <div className="mb-3">
+                                    <input type="password" className="form-control" disabled={this.state.disableEdit} id="repeatPassword" placeholder="Repetir nueva contraseña" />
+                                </div>
+                                <div className="mb-1">
+                                    <input type="text" className="form-control" disabled={this.state.disableEdit} value={this.state.user.direction} onChange={this.changeDirection} id="direction" placeholder="Dirección" />
+                                </div>
+                                <div className="mb-1">
+                                    <input type="text" className="form-control" disabled={this.state.disableEdit} value={this.state.user.poblation} onChange={this.changePoblation} id="poblation" placeholder="Población" />
+                                </div>
+                                <div className="mb-1">
+                                    <input type="text" className="form-control" disabled={this.state.disableEdit} id="postalCode" value={this.state.user.postalCode} onChange={this.changePostalCode} placeholder="Código postal" />
+                                </div>
+                                <div className="mb-3">
+                                    <input type="text" className="form-control" disabled={this.state.disableEdit} id="province" value={this.state.user.province} onChange={this.changeProvince} placeholder="Provincia" />
+                                </div>
+                                <div className="text-center">
+                                    <button type="submit" disabled={this.state.disableEdit} className="btn btn-primary px-5 w-100">Actualizar datos</button>
+                                </div>
+                            </form>
+                            <div className="text-center">
+                                <button disabled={this.state.disableEdit} onClick={this.showDeleteAccountModal} className="btn btn-danger mt-2 px-5 w-100">Eliminar cuenta</button>
+                            </div>
+                        </BlueCard>
+
+                    <Modal show={this.state.showDeleteAccountModal} onHide={this.hideDeleteAccountModal}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Eliminar cuenta</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <p>¿Estas seguro de que deseas eliminar permanentemente tu cuenta? Una vez borrada no podrá ser recuperada</p>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <button className="btn btn-secondary" onClick={this.hideDeleteAccountModal}>
+                                Cancelar
+                            </button>
+                            <button className="btn btn-danger" onClick={this.deleteProfile}>
+                                Eliminar
+                            </button>
+                        </Modal.Footer>
+                    </Modal>
+                </>
+            )}
             </Layout>
         );
     }
--- src/services/user.services.js
+++ src/services/user.services.js
@@ -90,3 +90,82 @@ export async function getUserData(props) {
         return [];
     } 
 }
+
+export async function updateUserProfile(props, userData) {
+    var formBody = [];
+    for (var property in userData) {
+        var encodedKey = encodeURIComponent(property);
+        var encodedValue = encodeURIComponent(userData[property]);
+        formBody.push(encodedKey + "=" + encodedValue);
+    }
+
+    formBody = formBody.join("&");
+    const response = await fetch(`${backendURL}/api/user/`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+            "x-access-token": localStorage.getItem("auth")
+        },
+        body: formBody
+    }).catch(() => {
+        return null;
+    });
+
+    if (response !== null) {
+        if (response.status === 200) {
+            const data = await response.json();
+            message.success(data.message);
+            return true;
+        } else if (response.status === 401 || response.status === 403) {
+            message.error("Tu sesión ha caducado. Inicia sesión de nuevo");
+            logout(false);
+            return props.history.push("/login");
+        } else if (response.status === 400) {
+            const data = await response.json();
+            message.error(data.message);
+            return false;
+        } else {
+            message.error("Ha ocurrido un error, intentalo de nuevo más tarde");
+            return false;
+        }
+    } else {
+        message.error("Ha ocurrido un error, intentalo de nuevo más tarde");
+        return;
+    }
+}
+
+export async function deleteAccount(props) {
+    const response = await fetch(`${backendURL}/api/user/`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+            "x-access-token": localStorage.getItem("auth")
+        },
+    }).catch(() => {
+        return null;
+    });
+
+    if (response !== null) {
+        if (response.status === 200) {
+            const data = await response.json();
+            message.success(data.message);
+            localStorage.removeItem("auth");
+            localStorage.removeItem("name");
+            return props.history.push("/"); 
+        } else if (response.status === 401 || response.status === 403) {
+            message.error("Tu sesión ha caducado. Inicia sesión de nuevo");
+            logout(false);
+            return props.history.push("/login");
+        } else if (response.status === 400) {
+            const data = await response.json();
+            message.error(data.message);
+            return [];
+        } else {
+            message.error("Ha ocurrido un error, intentalo de nuevo más tarde");
+            return [];
+        }
+    } else {
+        message.error("Ha ocurrido un error, intentalo de nuevo más tarde");
+        return [];
+    } 
+}
