1f1368f599ea4706917c30048b1c569f241b27ca
Gonzalo Eizaguirre <gonzaloeizaguirre@opendeusto.es>
2022-05-11 13:24:56 +0200

    cambiar version de dependencias

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '../node_modules/antd/dist/antd.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'antd/dist/antd.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './assets/css/main.css'
 import AppRouter from './route';
